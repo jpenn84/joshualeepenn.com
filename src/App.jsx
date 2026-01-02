bb76b7ef52fd8689fb28db03e3f979cb34799a9c
Joshua Penn <joshualeepenn@gmail.com>
2026-01-02 14:42:30 -0500

    Move the main sections to a separate LandingPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Interests from './components/Interests';
+import LandingPage from './components/LandingPage';
 import Footer from './components/Footer';
-import Articles from "./components/Articles";
-import Links from './components/Links';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <main>
-        <Hero />
-        <Interests />
-        <Articles />
-        <Links />
+        <LandingPage />
       </main>
       <Footer />
     </div>
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from './Hero';
+import Interests from './Interests';
+import Articles from './Articles';
+import Links from './Links';
+
+const LandingPage = () => {
+  return (
+    <>
+      <Hero />
+      <Interests />
+      <Articles />
+      <Links />
+    </>
+  );
+};
+
+export default LandingPage;
